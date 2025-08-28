6fdb4ee2e111d6a49e1b42f3d64356a1a46bbf85
Pavitra Golchha <hello@pavi2410.com>
2025-08-29 02:35:22 +0530

    Enhance install script with Codespaces support

--- install.sh
+++ install.sh
@@ -10,4 +10,16 @@ fi
 
 mise --version
 mise trust .
+
+if [[ -d "$CODESPACES_VSCODE_FOLDER" ]]; then
+    mise trust "$CODESPACES_VSCODE_FOLDER"
+fi
+
 mise install
+
+eval "$(~/.local/bin/mise activate bash)"
+
+mise dr
+
+eval "$(starship init bash)"
+
