17c7fe36e34757b5ba8971deb476d88b1fca1f73
Pavitra Golchha <hello@pavi2410.com>
2025-08-29 02:49:08 +0530

    Refactor install.sh to simplify setup process

--- install.sh
+++ install.sh
@@ -11,15 +11,9 @@ fi
 mise --version
 mise trust .
 
-if [[ -d "$CODESPACES_VSCODE_FOLDER" ]]; then
-    mise trust "$CODESPACES_VSCODE_FOLDER"
-fi
-
 mise install
-
-eval "$(~/.local/bin/mise activate bash)"
 
 mise dr
 
-eval "$(starship init bash)"
-
+BASHRC="$(pwd)/.bashrc"
+echo 'source "$BASHRC"' >> ~/.bashrc
